83870830add0b7a67d7fbedb4cbbc9df8129a1d8
Cristian Iván Aráoz <60581700+crisaraoz@users.noreply.github.com>
2022-07-05 20:34:44 -0300

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por <a href="https://github.com/crisaraoz">Cris Aráoz</a>
+    </div>
+    <div>
+      <a href="https://www.linkedin.com/in/cristian-araoz-b1429941/">
+        Work with me!
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
